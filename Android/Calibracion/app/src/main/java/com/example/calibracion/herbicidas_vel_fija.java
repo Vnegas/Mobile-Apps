7983cc2c97b11de1685a4b1ba70704aac915add9
Vnegas <sebas.venegas.brenes@gmail.com>
2024-09-07 22:06:37 -0600

    Update: Added option to calculate in km/h

--- Android/Calibracion/app/src/main/java/com/example/calibracion/herbicidas_vel_fija.java
+++ Android/Calibracion/app/src/main/java/com/example/calibracion/herbicidas_vel_fija.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +19,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class herbicidas_vel_fija extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class herbicidas_vel_fija extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Button calcular;
     ImageView herbIcon;
     ImageView fungIcon;
     ImageView dosIcon;
+    Spinner spinner;
+    boolean hasResult;
+    float result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +77,37 @@ public class herbicidas_vel_fija extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> medidas = new ArrayList<String>();
+        medidas.add("(m/s)");
+        medidas.add("(km/h)");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.spinner_layout, medidas);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+        hasResult = false;
+        result = 0;
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (hasResult) {
+            calculate(null);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) { }
 
     float readNumber(int id) {
         return Float.parseFloat(((EditText)findViewById(id)).getText().toString());
@@ -78,7 +118,7 @@ public class herbicidas_vel_fija extends AppCompatActivity {
     }
 
     public void calculate(View view) {
-        int all = 0;
+        int all = 0; result = 0;
         float descarga = 0; float ancho = 0; float vel = 0;
         if (!isEmpty(R.id.inputDescarga)) {
             descarga = readNumber(R.id.inputDescarga);
@@ -112,12 +152,18 @@ public class herbicidas_vel_fija extends AppCompatActivity {
         }
 
         if (all == 3) {
-            float result = ((descarga * 10000) / (vel * 60)) / ancho;
+            if (String.valueOf(spinner.getSelectedItem()).equals("(m/s)")) {
+                result = ((descarga * 10000) / (vel * 60)) / ancho;
+            } else {
+                result = (float) (((descarga * 10000) / ((vel / 3.6) * 60)) / ancho);
+            }
             ((TextView)findViewById(R.id.textResultado)).setText(Float.toString(result));
+            hasResult = true;
         } else {
             ((TextView)findViewById(R.id.textResultado)).setText("");
             ((TextView)findViewById(R.id.textResultado)).setHint("Resultado");
             ((TextView)findViewById(R.id.textResultado)).setHintTextColor(Color.parseColor("#FF000000"));
+            hasResult = false;
         }
     }
 }
--- Android/Calibracion/app/src/main/java/com/example/calibracion/herbicidas_vol_fijo.java
+++ Android/Calibracion/app/src/main/java/com/example/calibracion/herbicidas_vol_fijo.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +19,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class herbicidas_vol_fijo extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class herbicidas_vol_fijo extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Button calcular;
     ImageView herbIcon;
     ImageView fungIcon;
     ImageView dosIcon;
+    Spinner spinner;
+    boolean hasResult;
+    float result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +77,37 @@ public class herbicidas_vol_fijo extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> medidas = new ArrayList<String>();
+        medidas.add("m/s");
+        medidas.add("km/h");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.spinner_layout_result, medidas);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+        hasResult = false;
+        result = 0;
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (hasResult) {
+            calculate(null);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) { }
 
     float readNumber(int id) {
         return Float.parseFloat(((EditText)findViewById(id)).getText().toString());
@@ -78,7 +118,7 @@ public class herbicidas_vol_fijo extends AppCompatActivity {
     }
 
     public void calculate(View view) {
-        int all = 0;
+        int all = 0; result = 0;
         float descarga = 0; float ancho = 0; float vol = 0;
         if (!isEmpty(R.id.inputDescarga)) {
             descarga = readNumber(R.id.inputDescarga);
@@ -119,12 +159,17 @@ public class herbicidas_vol_fijo extends AppCompatActivity {
         }
 
         if (all == 3) {
-            float result = ((10000 / ancho) / (vol / descarga)) / 60;
+            result = ((10000 / ancho) / (vol / descarga)) / 60;
+            if (String.valueOf(spinner.getSelectedItem()).equals("km/h")) {
+                result = (float) (result * 3.6);
+            }
             ((TextView)findViewById(R.id.textResultado)).setText(Float.toString(result));
+            hasResult = true;
         } else {
             ((TextView)findViewById(R.id.textResultado)).setText("");
             ((TextView)findViewById(R.id.textResultado)).setHint("Resultado");
             ((TextView)findViewById(R.id.textResultado)).setHintTextColor(Color.parseColor("#FF000000"));
+            hasResult = false;
         }
     }
 }
